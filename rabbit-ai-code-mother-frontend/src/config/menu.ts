382aa267e472066305041e42cd0f3c6c0b8316a4
Fantasy <1362724990@qq.com>
2025-08-14 22:28:23 +0800

    feat: 前端初始化完成 1. 全局通用布局 2. 路由跳转和高亮同步 3. 全局自定义请求 + 前端代码生成器 4. 清理多余代码

---
+++ rabbit-ai-code-mother-frontend/src/config/menu.ts
@@ -0,0 +1,9 @@
+export type SimpleMenuItem = { key: string; label: string }
+
+const menuConfig: SimpleMenuItem[] = [
+	{ key: '/', label: '首页' },
+	{ key: '/about', label: '关于' },
+]
+
+export default menuConfig
+
