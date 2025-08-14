cb7c8856c19f9e6ed1a3476abab8df229426081b
Fantasy <1362724990@qq.com>
2025-08-14 20:34:31 +0800

    feat: 后端初始化 1. 初始化项目 2. 添加依赖和配置（HuTool 和 Knife4j 接口文档） 3. 提供基础代码（自定义异常、请求响应包装类） 4. 提供基础接口（健康检查接口）

---
+++ src/main/java/com/fantasy/rabbitaicodemother/RabbitAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.rabbitaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitAiCodeMotherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fantasy/rabbitaicodemother/RabbitAiCodeMotherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.rabbitaicodemother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitAiCodeMotherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
