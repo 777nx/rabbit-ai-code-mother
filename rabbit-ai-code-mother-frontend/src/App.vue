beb1ae40b8c5c5dab28af03c4787f31069b29bd2
Fantasy <1362724990@qq.com>
2025-08-14 22:34:42 +0800

    add: Ant Design Vue 组件全局改为默认中文

--- rabbit-ai-code-mother-frontend/src/App.vue
+++ rabbit-ai-code-mother-frontend/src/App.vue
@@ -3,13 +3,20 @@ import BasicLayout from '@/layouts/BasicLayout.vue'
 
 import { healthCheck } from '@/api/healthController.ts'
 
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
+
 healthCheck().then((res) => {
   console.log(res)
 })
 </script>
 
 <template>
-  <BasicLayout />
+  <a-config-provider :locale="zhCN">
+    <BasicLayout />
+  </a-config-provider>
 </template>
 
 <style></style>
