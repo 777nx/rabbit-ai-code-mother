ef81fa3f482ebd2b56f5094c9358be23e4e3f539
Fantasy <1362724990@qq.com>
2025-08-16 16:01:21 +0800

    feat: 应用模块（完成基本功能） 1. AI 生成基础功能代码 2. 完成基本功能、人工测试通过

--- src/main/java/com/fantasy/rabbitaicodemother/service/AppService.java
+++ src/main/java/com/fantasy/rabbitaicodemother/service/AppService.java
@@ -1,7 +1,12 @@
 package com.fantasy.rabbitaicodemother.service;
 
+import com.fantasy.rabbitaicodemother.model.dto.app.AppQueryRequest;
+import com.fantasy.rabbitaicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.fantasy.rabbitaicodemother.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,27 @@ import com.fantasy.rabbitaicodemother.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用封装类
+     *
+     * @param app 应用
+     * @return 应用封装类
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装列表
+     *
+     * @param appList 应用列表
+     * @return 应用封装列表
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest 应用查询请求
+     * @return 应用查询条件
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
