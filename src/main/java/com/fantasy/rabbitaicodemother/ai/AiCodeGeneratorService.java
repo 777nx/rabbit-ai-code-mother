24504b371856c0cd281f0d61cda976811bba7e2c
Fantasy <1362724990@qq.com>
2025-08-17 21:15:33 +0800

    feat: 工程项目生成（工具调用流式输出） 1. 定制 LangChain4j 源码，给 TokenStream 增加工具调用流式输出 2. 定义统一的流式消息格式 3. 修改 AI 门面服务，通过适配器将 Vue 生成模式的 TokenStream 转为 Flux（同时修改了 AI 方法的返回值为 TokenStream） 4. 定义了消息流处理器，支持对不同模式消息进行统一处理 5. 简化 AppServiceImpl 和 AI 对话的代码

--- src/main/java/com/fantasy/rabbitaicodemother/ai/AiCodeGeneratorService.java
+++ src/main/java/com/fantasy/rabbitaicodemother/ai/AiCodeGeneratorService.java
@@ -4,6 +4,7 @@ import com.fantasy.rabbitaicodemother.ai.model.HtmlCodeResult;
 import com.fantasy.rabbitaicodemother.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.TokenStream;
 import dev.langchain4j.service.UserMessage;
 import reactor.core.publisher.Flux;
 
@@ -55,5 +56,5 @@ public interface AiCodeGeneratorService {
      * @return AI 的输出结果
      */
     @SystemMessage(fromResource = "prompt/codegen-vue-project-system-prompt.txt")
-    Flux<String> generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
+    TokenStream generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
 }
