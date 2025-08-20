5a6cce2e81264e1ebc8ce82239accac68ec16470
Fantasy <1362724990@qq.com>
2025-08-20 17:34:57 +0800

    feat: 系统优化（实时性优化） 1. Vue 工程异步构建改为同步构建，构建完成后才结束 SSE 连接

--- src/main/java/com/fantasy/rabbitaicodemother/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/fantasy/rabbitaicodemother/core/handler/JsonMessageStreamHandler.java
@@ -6,8 +6,6 @@ import cn.hutool.json.JSONUtil;
 import com.fantasy.rabbitaicodemother.ai.model.message.*;
 import com.fantasy.rabbitaicodemother.ai.tools.BaseTool;
 import com.fantasy.rabbitaicodemother.ai.tools.ToolManager;
-import com.fantasy.rabbitaicodemother.constant.AppConstant;
-import com.fantasy.rabbitaicodemother.core.builder.VueProjectBuilder;
 import com.fantasy.rabbitaicodemother.model.entity.User;
 import com.fantasy.rabbitaicodemother.model.enums.ChatHistoryMessageTypeEnum;
 import com.fantasy.rabbitaicodemother.service.ChatHistoryService;
@@ -26,9 +24,6 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
-
-    @Resource
-    private VueProjectBuilder vueProjectBuilder;
 
     @Resource
     private ToolManager toolManager;
@@ -60,8 +55,6 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
